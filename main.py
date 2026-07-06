9aff73de56d3ea56f59999ed399d866349361406
olad46467-rgb <olad46467@gmail.com>
2026-07-06 07:26:33 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print("🤖 TradeLearn Bot is starting...")
+
+bot_name = "TradeLearnBot"
+channel_name = "TradeLearn Hub"
+
+print("Bot Name:", bot_name)
+print("Channel:", channel_name)
+print("Welcome to", channel_name)
