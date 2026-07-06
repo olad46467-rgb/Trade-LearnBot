9584f95ff44bf82eb5d1ddacfb9bea22b76444fa
olad46467-rgb <olad46467@gmail.com>
2026-07-06 09:13:37 -0700

    Update menu.py

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -1,1 +1,18 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+join_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="📢 Join Channel",
+                url="https://t.me/TradeLearn Hub"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="✅ Verify",
+                callback_data="verify"
+            )
+        ]
+    ]
+)
